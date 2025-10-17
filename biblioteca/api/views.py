f9da9801a3ae23699ac2a4d40c2673c80212eaeb
Jhonathan <jhonathan.ludena@unl.edu.ec>
2025-10-17 09:49:47 -0500

    Mejora de Vistas Iniciales
    17/10/2025

--- biblioteca/api/views.py
+++ biblioteca/api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from api.models import Persona, Cuenta, MaterialLectura, Libro, Manga, Novela, RegistroLectura
+from .serializers import PersonaSerializer, CuentaSerializer, LibroSerializer, MangaSerializer, NovelaSerializer, RegistroLecturaSerializer
 
-# Create your views here.
+
+
+
+class PersonaViewSet(viewsets.ModelViewSet):
+    queryset = Persona.objects.all()
+    serializer_class = PersonaSerializer
+
+class CuentaViewSet(viewsets.ModelViewSet):
+    queryset = Cuenta.objects.all()
+    serializer_class = CuentaSerializer
+
+class LibroViewSet(viewsets.ModelViewSet):
+    queryset = Libro.objects.all()
+    serializer_class = LibroSerializer
+
+class MangaViewSet(viewsets.ModelViewSet):
+    queryset = Manga.objects.all()
+    serializer_class = MangaSerializer
+
+class NovelaViewSet(viewsets.ModelViewSet):
+    queryset = Novela.objects.all()
+    serializer_class = NovelaSerializer
+
+class RegistroLecturaViewSet(viewsets.ModelViewSet):
+    queryset = RegistroLectura.objects.all()
+    serializer_class = RegistroLecturaSerializer
