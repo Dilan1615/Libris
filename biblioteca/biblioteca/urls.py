d299a320f2a9b9d25ca31abf61ed3303be178cb9
Sebastian <joseph.narvaez@unl.edu.ec>
2025-10-19 18:18:34 -0500

    Acoplamiento de mitigaciones en urls y views

--- biblioteca/biblioteca/urls.py
+++ biblioteca/biblioteca/urls.py
@@ -1,8 +1,16 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from api.views import LibroViewSet, MangaViewSet, NovelaViewSet, RegisterView, LoginView, ProfileView, RegistroLecturaViewSet
+
+router = DefaultRouter()
+router.register('libros', LibroViewSet)
+router.register('mangas', MangaViewSet)
+router.register('novelas', NovelaViewSet)
+router.register('registros-lectura', RegistroLecturaViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.api.urls'))
+    path('api/register/', RegisterView.as_view()),
+    path('api/login/', LoginView.as_view()),
+    path('api/profile/', ProfileView.as_view()),
+] + router.urls
 
-]
